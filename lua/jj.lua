efef83a1f0da7f8b33a8ef7bdf74b4b196831bcb
sivansh11 <sivanshgupta2002@gmail.com>
2025-11-22 14:27:29 +0530

    added edit

--- lua/jj.lua
+++ lua/jj.lua
@@ -5,7 +5,41 @@ local utils = require('utils')
 local config = {
 }
 
-local function jj_log_keymaps(state)
+-- jj edit
+function M.jj_edit(state, ignore_immutable)
+  local change_id = utils.get_change_id()
+  if not change_id then
+    vim.notify("Change ID not found", vim.log.levels.ERROR)
+    return
+  end
+
+  local cmd = "jj edit -r " .. change_id
+  if ignore_immutable then
+    cmd = cmd .. " --ignore-immutable"
+  end
+
+  local _, success = utils.run(cmd)
+  if not success then
+    vim.notify("Edit " .. change_id .. " failed", vim.log.levels.ERROR)
+  end
+
+  vim.notify("Editing " .. change_id, vim.log.levels.INFO)
+
+  vim.cmd('checktime')
+
+  local win = vim.fn.bufwinid(state.buf)
+  local cursor_pos
+  if win ~= -1 then
+    cursor_pos = vim.api.nvim_win_get_cursor(win)
+  end
+
+  M.jj_log()
+
+  win = vim.fn.bufwinid(state.buf)
+  vim.api.nvim_win_set_cursor(win, cursor_pos)
+end
+
+function M.jj_log_keymaps(state)
   -- Close jj-log
   vim.keymap.set('n', '<Esc>', function()
     vim.api.nvim_buf_delete(state.buf, { force = true })
@@ -22,6 +56,20 @@ local function jj_log_keymaps(state)
     desc = "Close jj buffer"
   })
 
+  -- Edit
+  vim.keymap.set('n', 'e', function()
+    M.jj_edit(state, false)
+  end, {
+    buffer = state.buf,
+    desc = "Edit"
+  })
+  vim.keymap.set('n', 'e', function()
+    M.jj_edit(state, true)
+  end, {
+    buffer = state.buf,
+    desc = "Edit(immutable)"
+  })
+
   local disabled_keys = { "i", "c", "a" }
   for _, key in ipairs(disabled_keys) do
     vim.keymap.set({ "n", "v" }, key, function() end, {
@@ -32,7 +80,7 @@ local function jj_log_keymaps(state)
 end
 
 function M.jj_log()
-  utils.run_and_display("jj log --no-pager", "jj-log", jj_log_keymaps)
+  utils.run_and_display("jj log --no-pager", "jj-log", M.jj_log_keymaps)
 end
 
 function M.setup(user_config)
--- lua/utils.lua
+++ lua/utils.lua
@@ -39,4 +39,60 @@ function M.run_and_display(cmd, name, set_keymaps_callback)
   end
 end
 
+-- Run command and return its results
+function M.run(cmd, input)
+  local output
+  if input then
+    output = vim.fn.system(cmd, input)
+  else
+    output = vim.fn.system(cmd)
+  end
+  local success = vim.v.shell_error == 0
+
+  if not success then
+    return "", false
+  end
+
+  return output, success
+end
+
+-- Get Change ID in line
+function M.get_change_id_in_line(line)
+  -- The change ID is the first word after the symbols and spaces
+  local change_id = line:match("^[%s│├─╯○◆×@]+%s+([a-z]+)")
+
+  if not change_id then
+    return nil
+  end
+
+  -- Verify the change ID exists by checking if jj log -r returns output
+  local _, success = M.run("jj log -r " .. change_id)
+  if success then
+    return change_id
+  end
+
+  return nil
+end
+
+-- get change id in the current line or the line above
+function M.get_change_id()
+  local line = vim.api.nvim_get_current_line()
+  local change_id = M.get_change_id_in_line(line)
+  if not change_id then
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local current_line_num = cursor_pos[1]
+
+    local line_above_num = current_line_num - 1
+    if line_above_num < 1 then
+      vim.notify("EasyJJ: change_id not found", vim.log.levels.ERROR)
+      return
+    end
+
+    local line_above = vim.api.nvim_buf_get_lines(0,
+      line_above_num - 1, line_above_num, false)[1]
+    change_id = M.get_change_id_in_line(line_above)
+  end
+  return change_id
+end
+
 return M
