8014804441395b36239a7d6bc1eddc5e56b4f46e
sivansh11 <sivanshgupta2002@gmail.com>
2025-11-22 15:11:16 +0530

    added new

--- lua/jj.lua
+++ lua/jj.lua
@@ -91,6 +91,38 @@ function M.jj_redo(state)
   vim.api.nvim_win_set_cursor(win, cursor_pos)
 end
 
+-- jj new
+function M.jj_new(state)
+  local change_id = utils.get_change_id()
+  if not change_id then
+    vim.notify("Change ID not found", vim.log.levels.ERROR)
+    return
+  end
+
+  local cmd = "jj new -r " .. change_id
+
+  local output, success = utils.run(cmd)
+  if not success then
+    vim.notify("EasyJJ: new not successful", vim.log.levels.ERROR)
+    vim.notify(output, vim.log.levels.ERROR)
+  end
+
+  vim.notify("EasyJJ: new", vim.log.levels.INFO)
+
+  vim.cmd('checktime')
+
+  local win = vim.fn.bufwinid(state.buf)
+  local cursor_pos
+  if win ~= -1 then
+    cursor_pos = vim.api.nvim_win_get_cursor(win)
+  end
+
+  M.jj_log()
+
+  win = vim.fn.bufwinid(state.buf)
+  vim.api.nvim_win_set_cursor(win, cursor_pos)
+end
+
 function M.jj_log_keymaps(state)
   -- Close jj-log
   vim.keymap.set('n', '<Esc>', function()
@@ -136,6 +168,14 @@ function M.jj_log_keymaps(state)
   end, {
     buffer = state.buf,
     desc = "Redo"
+  })
+
+  -- New
+  vim.keymap.set('n', 'n', function()
+    M.jj_new(state)
+  end, {
+    buffer = state.buf,
+    desc = "New"
   })
 
   local disabled_keys = { "i", "c", "a" }
